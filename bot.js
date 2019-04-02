fb47c30ab0b3d4325e5288b1cfaa98baa203580a
PSIClipse <49186402+PSIClipse@users.noreply.github.com>
2019-04-02 17:02:17 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,16 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
 client.on('message', message => {
     if (message.content === 'Hello Dealer') {
     	message.reply('What the fuck did you just fucking say about me, you little bitch?');
