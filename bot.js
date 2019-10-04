2559d3e0b7a62815d594e18a184d3fd3a3444409
PSIClipse <49186402+PSIClipse@users.noreply.github.com>
2019-10-04 13:16:28 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
  
 client.on('message', message => {
-    if (message.content === 'Hello Dealer') {
+    if (message.content === 'hey DKBot') {
     	message.reply('What the fuck did you just fucking say about me, you little bitch?');
   	}
 });
@@ -19,20 +19,6 @@ client.on('message', message => {
 });
 
 
-client.on('message', message => {
-    if (message.content === 'clip') {
-    	message.channel.send(' jdplay  https://www.youtube.com/watch?v=EDr_krEQNrU&list=PL1qbU9E01marbo3zTcHp29aYCgZh8S0zB');
-    }
-});
-    client.on('message', message => {
-    // If the message is '!rip'
-    if (message.content === '!rip') {
-        // Create the attachment using Attachment
-        const attachment = new Attachment('https://i.imgur.com/w3duR07.png');
-        // Send the attachment in the message channel
-        message.channel.send(attachment);
-    }
-});
 
 client.on('message', message => {
     if (message.content === '100 foreplay events') {
@@ -41,7 +27,10 @@ client.on('message', message => {
 });
 
 
-
+bot.on('message' message => {
+    if (message.content === '100 foreplay events') {
+    message.channel.send("My Bot's message", {files: ["https://i.imgur.com/XxxXxXX.jpg"]});
+});
 
 
 
