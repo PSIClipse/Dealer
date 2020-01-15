c16c63316377d09122c6a26e14daa02773a5cf5e
PSIClipse <49186402+PSIClipse@users.noreply.github.com>
2020-01-15 16:05:41 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,20 +20,6 @@ client.on('message', message => {
 
 
 
-client.on('message', message => {
-    if (message.content === '100 foreplay events') {
-    	message.reply('https://discord.gg/CjH6dby');
-  	}
-});
-
-
-client.on('message' message => {
-    if (message.content === '100 foreplay events') {
-        message.reply('https://discord.gg/CjH6dby', {files: ['https://cdn.discordapp.com/attachments/521195716006117376/629474123797626894/0.png']});
-});
-
-
-
 
 
 // THIS  MUST  BE  THIS  WAY
